32ce8043e11375dc88f0524f1024ccfb3e425e20
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-06-27 10:59:37 +0300

    Added /etc/resolv.conf management for Samba AD DC
    Samba AD DC can't use DHCP so /etc/resolv.conf has to be managed manually.
    Moreover, Samba AD DC node requires access to the DNS entries stored in Samba's
    internal DNS server running on localhost.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/server/config/dc.pp
+++ manifests/server/config/dc.pp
@@ -14,13 +14,19 @@ class samba4::server::config::dc
 
 ) inherits samba4::params
 {
-
     # Convert the parameters into upper- and lowercase
     $domain_lc = downcase($domain)
     $domain_uc = upcase($domain)
     $realm_lc = downcase($realm)
     $realm_uc = upcase($realm)
 
+    # Add static resolv.conf
+    class { '::resolv_conf':
+        nameservers => ['127.0.0.1'],
+        domainname => $realm_lc,
+    }
+
+    # Provision the domain
     exec { 'samba4-domain-provisioning':
         command => "samba-tool domain provision --use-rfc2307 --dns-backend=SAMBA_INTERNAL --adminpass=\"${adminpass}\" --realm=${realm_uc} --domain=${domain_uc} --server-role=${server_role} --host-ip=${host_ip} --host-name=${host_name}",
         path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
