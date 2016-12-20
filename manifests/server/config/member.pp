a109d9498367a7c825f71b69c8f62d93b0f6ca7f
Samuli Seppänen <samuli.seppanen@gmail.com>
2016-12-20 16:52:50 +0200

    Do not rejoin member servers to the domain unnecessarily
    The earlier check based on "host" command did not work reliably, and was broken
    in general. The new check makes an LDAP query to determine if the member server
    is already in Samba AD before attempting to join the domain. This prevents the
    "net ads join" command from hanging if the machine account is already present.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/server/config/member.pp
+++ manifests/server/config/member.pp
@@ -18,6 +18,14 @@ class samba4::server::config::member
     $domain_uc = upcase($domain)
     $host_name_lc = downcase($host_name)
 
+    # Convert the realm into LDAP format:
+    #
+    # smb.domain.com -> DC=smb,DC=domain,DC=com
+    #
+    # The second regsubst strips out the trailing ','
+    $pre = regsubst($realm_lc, '(^|[.])([a-zA-Z0-9_-]+)', 'DC=\2,', 'G')
+    $rdn = regsubst($pre, ',$', '')
+
     # Concat is used here because it seems the only reasonable option:
     #
     # - Samba does not support including all files in a directory (e.g.
@@ -58,7 +66,7 @@ class samba4::server::config::member
     exec { 'samba4-net-ads-join':
         command => "net ads join -U administrator%${adminpass}",
         path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-        unless  => "host ${host_name_lc}.${realm_lc}",
+        unless  => "net ads dn \'CN=${host_name_lc},CN=Computers,${rdn}\' cn -Uadministrator%${adminpass}|grep cn: ",
         require => Class['nsswitch'],
     }
 
